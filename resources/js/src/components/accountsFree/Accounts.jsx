fc4166ebc1a17cb25df86110b65db7f9a12b44e7
jose dev <josededanes99@gmail.com>
2022-02-15 12:41:34 -0600

    Disabled Server new york

--- resources/js/src/components/accountsFree/Accounts.jsx
+++ resources/js/src/components/accountsFree/Accounts.jsx
@@ -7,7 +7,7 @@ export default function Accounts() {
     return (
 
         <ListAccount title={"Servidores gratis 3 días"}>
-            <Usa1 />
+            { /*<Usa1 /> */}
             <Toronto />
         </ListAccount>
     )
